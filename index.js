b1019992bd9b1a161bd9c49235b4b4fc0d6312ab
gabriruf <grufinosouza@tutanota.com>
2026-07-31 10:28:56 -0300

    feat: the base files for fetching JSON data from GitHub

---
+++ index.js
@@ -0,0 +1,54 @@
+"use strict";
+import { loadEnvFile, argv, env } from 'process';
+import https from 'https';
+import fs from 'fs/promises';
+
+loadEnvFile('.env');
+
+const GITHUB_USER = argv[2];
+const USER_AGENT = GITHUB_USER;
+const API_KEY = env.API_KEY ?? "no api key found";
+const URL = 'api.github.com';
+const OPTS = {
+    hostname: URL,
+    method: 'GET',
+    path: `/users/${GITHUB_USER}/events/public`,
+    headers: {
+        'User-Agent': USER_AGENT,
+        'Authorization': `Bearer ${API_KEY}`,
+        'Accept': 'application/vnd.github+json',
+        'X-GitHub-Api-Version': '2026-03-10'
+    } 
+}
+
+async function createFile(filename, content) {
+    try {
+        await fs.writeFile(filename, JSON.stringify(content, null, 2),'utf8');
+    } catch (err) {
+        console.log("Error on writing content to file: ", err)
+    } finally {
+        console.log("--------------------");
+        console.log(" Operation finished ");
+        console.log("--------------------");
+    }
+}
+
+https
+    .get(OPTS, res => {
+        let data = '';
+
+        res.on('data', chunk => {
+            data += chunk;
+        })
+
+        res.on('end', () => {
+            let info = JSON.parse(data);
+            const filename = argv[3];
+            console.log(info);
+            
+            createFile(filename, info);
+        })
+    })
+    .on('error', err => {
+        console.log(`Error: ${err.message}`);
+    })
